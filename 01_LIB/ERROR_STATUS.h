399c5268e2ea5a9de19cc3cab967be325720b6f3
Karem <hodazamel1112@gmail.com>
2022-12-13 15:35:58 +0200

    STM32F103C6 LIB

---
+++ 01_LIB/ERROR_STATUS.h
@@ -0,0 +1,25 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    ERROR_STATUS.h    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ *  Author  : Mahmoud Karem Zamel
+ *  Date    : 29 June 2021
+ *  Vertion : V01
+ *  Layer   : BIN/Services/LIB
+ *
+ */ 
+
+#ifndef ERROR_STATUS_H_
+#define ERROR_STATUS_H_
+
+typedef enum{
+
+	OK,
+	NOK,
+	OUT_OF_RANGE,
+	NULL_POINTER
+
+}ErrorStatus;
+
+
+
+#endif /* ERROR_STATUS_H_ */
